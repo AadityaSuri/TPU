feb4212f84a4d98ec52d274e7442f7dae74b4621
Aaditya Suri <aadityasuri1@gmail.com>
2024-05-19 18:35:51 -0400

    fixed point MAC working properly, testbenched

--- MAC.sv
+++ MAC.sv
@@ -1,13 +1,44 @@
-module MAC(input logic clk, input logic rst, input logic en,
-           input logic signed [15:0] A, input logic signed [15:0] B, 
-           output logic signed [31:0] out);
+// module MAC(input logic clk, input logic rst, input logic en,
+//            input logic signed [15:0] A, input logic signed [15:0] B, 
+//            output logic signed [31:0] out);
 
-    always @ (posedge clk or posedge rst) begin
-        if (rst)   
-            out <= 32'b0;
-        else
-            out <= en ? out + A*B : out;
+//     always @ (posedge clk or posedge rst) begin
+//         if (rst)   
+//             out <= 32'b0;
+//         else
+//             out <= en ? out + A*B : out;
+//     end
+
+
+// endmodule: MAC
+
+module MAC(
+    input logic clk,
+    input logic rst,
+    input logic en,
+    input logic signed [31:0] A, // 16-bit signed fixed-point number
+    input logic signed [31:0] B, // 16-bit signed fixed-point number
+    output logic signed [31:0] out // 32-bit signed fixed-point accumulator
+);
+
+    localparam Q = 10;
+    localparam N = 32;
+    
+    logic signed [N-1:0] acc_sum;
+    logic signed [N-1:0] product;
+    logic ovr;
+
+    qmult #(Q, N) mult(A, B, product, ovr);
+    qadd #(Q, N) add(out, product, acc_sum);
+
+    always_ff @ (posedge clk or posedge rst) begin
+        if (rst) begin
+            out <= 32'sd0;
+            acc_sum <= 32'sd0;
+        end else begin
+            // product <= (A * B) >>> 15; 
+            out <= en ? acc_sum : out;
+        end
     end
-
 
 endmodule: MAC
--- MAC_tb.sv
+++ MAC_tb.sv
@@ -1,76 +1,86 @@
-    module MAC_tb();
+`timescale 1 ps / 1 ps
+  
+    
+module MAC_tb();
 
-        parameter CLK_PERIOD = 10;
-        parameter RESET_DURATION = 10;
-        parameter RUN_DURATION = 10;
+    parameter CLK_PERIOD = 10;
+    parameter RESET_DURATION = 10;
+    parameter RUN_DURATION = 10;
 
-        // define signals
-        logic clk, rst, en;
-        logic signed [15:0] A, B;
-        logic signed [31:0] out;
+    // define signals
+    logic clk, rst, en;
+    logic signed [31:0] A, B;
+    logic signed [31:0] out;
 
-        // instantiate the DUT
-        MAC dut (.clk(clk), .rst(rst), .en(en), .A(A), .B(B), .out(out));
+    // instantiate the DUT
+    MAC dut (.clk(clk), .rst(rst), .en(en), .A(A), .B(B), .out(out));
 
-        // generate clock
-        initial begin
-            clk = 0;
-            forever #(CLK_PERIOD/2) clk = ~clk;
+    // generate clock
+    initial begin
+        clk = 0;
+        forever #(CLK_PERIOD/2) clk = ~clk;
+    end
+
+    task apply_reset;
+        $display("Applying reset");
+
+        rst = 1;    
+        #RESET_DURATION
+        rst = 0;
+    endtask: apply_reset
+
+    task check_output(input signed [31:0] expected_value);
+        if (out !== expected_value) begin
+            $display("Test failed: Expected %d, got %d at time %t", expected_value, out, $time);
+        end else begin
+            $display("Test passed: Expected %d, got %d at time %t", expected_value, out, $time);
         end
+    endtask
 
-        task apply_reset;
-            $display("Applying reset");
+    task run_mac(input logic [31:0] in_A, input logic [31:0] in_B, input logic signed [31:0] expected_value);
+        A = in_A;
+        B = in_B;
 
-            rst = 1;    
-            #RESET_DURATION
-            rst = 0;
-        endtask: apply_reset
+        en = 0;
+        #RUN_DURATION
+        en = 1;
+        #RUN_DURATION
+        en = 0;
+        #RUN_DURATION
 
-        task check_output(input [31:0] expected_value);
-            if (out !== expected_value) begin
-                $display("Test failed: Expected %d, got %d at time %t", expected_value, out, $time);
-            end else begin
-                $display("Test passed: Expected %d, got %d at time %t", expected_value, out, $time);
-            end
-        endtask
-
-        task run_mac(input logic [15:0] in_A, input logic [15:0] in_B, input logic [15:0] expected_value);
-            A = in_A;
-            B = in_B;
-
-            en = 0;
-            #RUN_DURATION
-            en = 1;
-            #RUN_DURATION
-            en = 0;
-            #RUN_DURATION
-
-            check_output(expected_value);
-        endtask: run_mac
+        check_output(expected_value);
+    endtask: run_mac
 
 
-        initial begin
-            apply_reset();
-            check_output(16'd0);
-            run_mac(16'd1, 16'd2, 32'd2);
-            run_mac(16'd3, 16'd2, 32'd8);
-            run_mac(16'd5, 16'd5, 32'd33);
+    initial begin
+        apply_reset();
+        check_output(32'd0);
+        run_mac({1'b0, 21'd1, 10'd0}, {1'b0, 21'd2, 10'd0}, {1'b0, 21'd2, 10'd0});
+        run_mac({1'b0, 21'd3, 10'd0}, {1'b0, 21'd2, 10'd0}, {1'b0, 21'd8, 10'd0});
+        run_mac({1'b0, 21'd5, 10'd0}, {1'b0, 21'd5, 10'd0}, {1'b0, 21'd33, 10'd0});
 
-            apply_reset();
-            check_output(16'd0);
-            run_mac(16'd5, 16'd8, 32'd40);
-            run_mac(16'd2, 16'd3, 32'd46);
-            run_mac(16'd4, -16'd3, 32'd34);
-            run_mac(-16'd2, -16'd2, 32'd38);
+        apply_reset();
+        check_output(32'd0);
+        run_mac({1'b0, 21'd5, 10'd0}, {1'b0, 21'd8, 10'd0}, {1'b0, 21'd40, 10'd0});
+        run_mac({1'b0, 21'd2, 10'd0}, {1'b0, 21'd3, 10'd0}, {1'b0, 21'd46, 10'd0});
+        run_mac({1'b1, 21'd3, 10'd0}, {1'b0, 21'd4, 10'd0}, {1'b0, 21'd34, 10'd0});
+        run_mac({1'b1, 21'd2, 10'd0}, {1'b1, 21'd2, 10'd0}, {1'b0, 21'd38, 10'd0});
 
-            apply_reset();
-            check_output(16'd0);
-            run_mac(16'd1, -16'd1, -32'd1);
-            run_mac(16'd0, 16'd5, -32'd1);
-            run_mac(16'd4, -16'd5, -32'd21);
-            run_mac(16'd10, 16'd5, 32'd29);
+        apply_reset();
+        check_output(32'd0);
+        run_mac({1'b1, 21'd1, 10'd0}, {1'b1, 21'd1, 10'd0}, {1'b0, 21'd1, 10'd0});
+        run_mac({1'b0, 21'd5, 10'd0}, {1'b1, 21'd8, 10'd0}, {1'b1, 21'd39, 10'd0});
+        run_mac({1'b0, 21'd2, 10'd0}, {1'b1, 21'd3, 10'd0}, {1'b1, 21'd45, 10'd0});
+        run_mac({1'b0, 21'd5, 10'd0}, {1'b0, 21'd10, 10'd0}, {1'b0, 21'd5, 10'd0});
 
-            $finish;
-        end
+        apply_reset();
+        check_output(32'd0);
+        run_mac({1'b0, 21'd0, 10'b1000_0000_00}, {1'b0, 21'd0, 10'b1000_0000_00}, {1'b0, 21'd0, 10'b0100_0000_00});  // 0.5 * 0.5 = 0.25 + 0 = 0.25
+        run_mac({1'b0, 21'd0, 10'b0100_0000_00}, {1'b0, 21'd1, 10'b1000_0000_00}, {1'b0, 21'd0, 10'b1010_0000_00});  // 0.25 * 1.5 = 0.375 + 0.25 = 0.625
+        run_mac({1'b1, 21'd5, 10'b0001_1000_00}, {1'b0, 21'd3, 10'b0100_0000_00}, {1'b1, 21'd15, 10'b1110_1110_00}); 
+        run_mac({1'b1, 21'd2, 10'b0100_0000_00}, 32'd0, {1'b1, 21'd15, 10'b1110_1110_00});
 
-    endmodule: MAC_tb
+        $finish;
+    end
+
+endmodule: MAC_tb
